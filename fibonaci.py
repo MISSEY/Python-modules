b9a419a7734dfeee1b5a3fc3c1b12d8d2344b24d
Missey <skmgun@gmail.com>
2016-04-07 23:56:01 +0530

    1st commit

---
+++ fibonaci.py
@@ -0,0 +1,9 @@
+def fib(n):
+    a=0
+    b=1
+    for i in range (0,n):
+        if(n>0):
+            c=a+b
+            a=b
+            b=c
+            print c
---
+++ sal.py
@@ -0,0 +1,3 @@
+from fibonaci import fib
+n=int(raw_input("enter the n"))
+fib(n);
